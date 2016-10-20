309f492d94ad27e773d74b3758abb24e05d5c5a9
fishpaypay <fishpaypay@fanswoo.com>
2016-10-20 18:48:31 +0800

    indexphp& css content01 設定/vup y/ wj6

---
+++ views/index.php
@@ -0,0 +1,47 @@
+<?=$temp['header_up']?>
+<?=$temp['header_down']?>
+
+<head>
+	<script>
+	</script>
+<!-- 最新編譯和最佳化的 CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<!-- 選擇性佈景主題 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+<!-- 最新編譯和最佳化的 JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="content01">
+	<div class="content01 bg" align="center">
+		<img src="img/content01_bg.jpg" >
+		<div class="centerbox" >
+			<h1>M型社會變成L型社會</h1>
+			<p>你 依然是金錢制度下</p>
+			<p>無法翻身的那群人嗎?</p>
+	</div>	
+	
+  	</div>
+ <nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
+</body>
