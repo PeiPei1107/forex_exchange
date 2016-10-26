e8884342d44a26e5be62274c8aec77a2d62ddc8c
fishpaypay <fishpaypay@fanswoo.com>
2016-10-26 19:08:17 +0800

    content03 40% 細微調整content01

--- views/index.php
+++ views/index.php
@@ -17,126 +17,154 @@
 </head>
 <body>
   <!-- content01區塊 -->
-<div class="content01" id="content01">
-  	<div class="colorbg" >
-  		<img src="img/index/content01/content01_bg.jpg" ></div>
-    <div class="blackbg" >
-      <img src="img/index/content01/content01_blackbg.jpg" ></div>
-    <div class="people" >
-        <div class="one">
-          <img src="img/index/content01/bigman.png" >
+  <div class="content01" id="content01">
+    	<div class="colorbg" >
+    		<img src="img/index/content01/content01_bg.jpg" ></div>
+      <div class="blackbg" >
+        <img src="img/index/content01/content01_blackbg.jpg" ></div>
+      <div class="people" >
+          <div class="one">
+            <img src="img/index/content01/bigman.png" >
+          </div>
+          <div class="two">
+            <img src="img/index/content01/suitman.png" >
+          </div>
+          <div class="three">
+            <img src="img/index/content01/sitman.png" >
+          </div>
+          <div class="four">
+            <img src="img/index/content01/shirtman.png" >
+          </div>
+          <div class="five">
+            <img src="img/index/content01/woman.png" >
+          </div> 
         </div>
-        <div class="two">
-          <img src="img/index/content01/suitman.png" >
-        </div>
-        <div class="three">
-          <img src="img/index/content01/sitman.png" >
-        </div>
-        <div class="four">
-          <img src="img/index/content01/shirtman.png" >
-        </div>
-        <div class="five">
-          <img src="img/index/content01/woman.png" >
-        </div> 
-      </div>
-    <div class="centerbox">
-        <h1 class="fadein"><img src="img/index/content01/M.png" class="one">型社會變成<img src="img/index/content01/L.png" class="two">&nbsp&nbsp&nbsp型社會</h1>
+      <div class="centerbox">
+          <h1 class="fadein"><img src="img/index/content01/M.png" class="one">型社會變成<img src="img/index/content01/L.png" class="two">&nbsp&nbsp&nbsp型社會</h1>
 
-        <div class="text one">
-          <p>你</p><p>&nbsp</p><p>依</p><p>然</p><p>是</p><p>金</p><p>錢</p>
-        <p>制</p><p>度</p><p>下</p></br>
-       </div>
-        <div class="text two">
-          <p>無</p><p>法</p><p>翻</p><p>身</p><p>的</p><p>那</p><p>群</p>
-          <p>人</p><p>嗎?</p>
-        </div>
-    </div>
+          <div class="text one">
+            <p>你</p><p>&nbsp</p><p>依</p><p>然</p><p>是</p><p>金</p><p>錢</p>
+          <p>制</p><p>度</p><p>下</p></br>
+         </div>
+          <div class="text two">
+            <p>無</p><p>法</p><p>翻</p><p>身</p><p>的</p><p>那</p><p>群</p>
+            <p>人</p><p>嗎?</p>
+          </div>
+      </div>
   <!-- headerbar -->
-    <div class="header_center">
-      <img src="img/index/content01/headerbar.png">
-      <div class="nav">
-        <a href="#content01" class="li one">
-          <p>從這開始</p>
-        </a>
-        <a href="#content02" class="li">
-          <p>投資困境</p>
-        </a>
-        <a href="#content03" class="li">
-          <p>夢想實踐</p>
-        </a>
-        <a href="#content03" class="li">
-          <p>致富機會</p>
-        </a>
-        <a href="#content04" class="li">
-          <p>四大保證</p>
-        </a>
-        <a href="#content05" class="li">
-          <p>立即參加</p>
-        </a>
-      </div>
-    </div>
-</div>
-
-<div class="content02">
-  <div class="first">
-    <div class="moneybg one">
-        <img src="img/index/content02/content02_bg1.png"></div>  
-    <div class="money">
-      <div class="left front">
-        <img src="img/index/content02/left_money_big.png"></div>
-      <div class="right front">
-       <img src="img/index/content02/rgiht_money_big.png"></div>
-    <div class="centerbox">
-      <div class="wordbox">
-        <p class="one">想要投資賺錢，卻不知道如何</p>
-        <p class="two">踏出第一步?</p>
-        <p class="three">你&nbsp是否犯了以下</p>  
-        <p class="four">投資大忌?</p>
-      </div>
-    </div>
-  </div>
-</div>
-  <div class="second">
-    <div class="moneybg two">
-      <img src="img/index/content02/content02_bg2.png"></div>   
-      <div class="wordbox">
-        <div class="item one">
-            <div class="left"><p class="text-left">策</p></div>
-            <div class="right"><p class="text-right">策略僵化</p></div>   
-          <img src="img/index/content02/item_black.png">
+      <div class="header_center">
+        <img src="img/index/content01/headerbar01.png">
+        <div class="nav">
+          <a href="#content01" class="li one">
+            <p>從這開始</p>
+          </a>
+          <a href="#content02" class="li">
+            <p>投資困境</p>
+          </a>
+          <a href="#content03" class="li">
+            <p>夢想實踐</p>
+          </a>
+          <a href="#content03" class="li">
+            <p>致富機會</p>
+          </a>
+          <a href="#content04" class="li">
+            <p>四大保證</p>
+          </a>
+          <a href="#content05" class="li">
+            <p>立即參加</p>
+          </a>
         </div>
-        <div class="item two">
-            <div class="left"><p class="text-left">虧</p></div>
-            <div class="right"><p class="text-right">害怕虧損</p></div>     
-          <img src="img/index/content02/item_red.png">
-        </div> 
-        <div class="item three">
-            <div class="left"><p class="text-left">情</p></div>
-            <div class="right"><p class="text-right">情緒操作</p></div>      
-          <img src="img/index/content02/item_black.png">
-        </div>  
-        <div class="item four">
-            <div class="left"><p class="text-left">市</p></div>
-            <div class="right"><p class="text-right">誤判市場</p></div>      
-          <img src="img/index/content02/item_red.png">
-        </div> 
-        <div class="item five">
-            <div class="left"><p class="text-left">運</p></div>
-            <div class="right"><p class="text-right">只靠運氣</p></div>     
-          <img src="img/index/content02/item_black.png">
-        </div> 
       </div>
-    
   </div>
-  <div class="middlebar">
-    <img src="img/index/content02/middle_bar.png">
-    <div class="title">
-      <span>
-        人人都享有致富&nbsp與&nbsp實踐夢想的權利
-      </span>
+  <!-- content02 -->
+  <div class="content02">
+    <div class="first">
+      <div class="moneybg one">
+          <img src="img/index/content02/content02_bg1.png"></div>  
+      <div class="money">
+        <div class="left front">
+          <img src="img/index/content02/left_money_big.png"></div>
+        <div class="right front">
+         <img src="img/index/content02/rgiht_money_big.png"></div>
+        <div class="centerbox">
+          <div class="wordbox">
+            <p class="one">想要投資賺錢，卻不知道如何</p>
+            <p class="two">踏出第一步?</p>
+            <p class="three">你&nbsp是否犯了以下</p>  
+            <p class="four">投資大忌?</p>
+          </div>
+        </div>
+      </div>
+  </div>
+    <div class="second">
+      <div class="moneybg two">
+        <img src="img/index/content02/content02_bg2.png"></div>   
+        <div class="wordbox">
+          <div class="item one">
+              <div class="left"><p class="text-left">策</p></div>
+              <div class="right"><p class="text-right">策略僵化</p></div>   
+            <img src="img/index/content02/item_black.png">
+          </div>
+          <div class="item two">
+              <div class="left"><p class="text-left">虧</p></div>
+              <div class="right"><p class="text-right">害怕虧損</p></div>     
+            <img src="img/index/content02/item_red.png">
+          </div> 
+          <div class="item three">
+              <div class="left"><p class="text-left">情</p></div>
+              <div class="right"><p class="text-right">情緒操作</p></div>      
+            <img src="img/index/content02/item_black.png">
+          </div>  
+          <div class="item four">
+              <div class="left"><p class="text-left">市</p></div>
+              <div class="right"><p class="text-right">誤判市場</p></div>      
+            <img src="img/index/content02/item_red.png">
+          </div> 
+          <div class="item five">
+              <div class="left"><p class="text-left">運</p></div>
+              <div class="right"><p class="text-right">只靠運氣</p></div>     
+            <img src="img/index/content02/item_black.png">
+          </div> 
+        </div>
     </div>
-  </div>
-</div>
+    <div class="middlebar">
+      <img src="img/index/content02/middle_bar.png">
+      <div class="title">
+        <span>
+          人人都享有致富&nbsp與&nbsp實踐夢想的權利
+        </span>
+      </div>
+    </div>
+    
+  <!-- content03 -->  
+<!--   <div class="content03">
+    <div class="triangle">
+      <img src="img/index/content03/triangle.png">
+      <img src="img/index/content03/plane.png">
+      <p></p>
+    </div>
+    <div class="block one">
+      <img src="img/index/content03/use_computer.jpg" class="bg">
+      <img src="" class="line">
+      <div class="wordbox">
+        <h1></h1>
+      </div>
+    </div>
+    <div class="block two">
+      <img src="img/index/content03/Young_bg.jpg" class="bg">
+      <img src="" class="line">
+      <div class="wordbox">
+        <h1></h1>
+      </div>
+    </div>
+    <div class="block three">
+      <img src="img/index/content03/businessman.jpg" class="bg">
+      <div class="wordbox">
+        <h1></h1>
+      </div>
+    </div>
+  </div> -->
+
 
 </body>
 
